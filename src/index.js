fe8dc960ebd4827b33413576e8e2253322a5949c
lahou <lahou.moustafa@hotmail.fr>
2021-06-06 12:56:12 +0100

    finished front, data left

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/portfolio-react">
       <App />
     </BrowserRouter>  
   </React.StrictMode>,
